6cb778a504377af108243b8ee0684c14d35dcf5a
Zachary DuBois <zacharydubois@icloud.com>
2015-01-24 04:23:07 -0500

    Add config.py

---
+++ config.py
@@ -0,0 +1,233 @@
+worlds["world"] = "/home/zacharydubois/render/worlds/world"
+worlds["nether"] = "/home/zacharydubois/render/worlds/world_nether"
+worlds["end"] = "/home/zacharydubois/render/worlds/world_the_end"
+worlds["hoth"] = "/home/zacharydubois/render/worlds/hoth"
+worlds["amp"] = "/home/zacharydubois/render/worlds/amp"
+worlds["oldworld"] = "/home/zacharydubois/render/worlds/old_world"
+worlds["oldnether"] = "/home/zacharydubois/render/worlds/old_world_nether"
+worlds["oldend"] = "/home/zacharydubois/render/worlds/old_world_the_end"
+
+
+
+renders["worldday"] = {
+    "world": "world",
+    "title": "World Daytime",
+    "rendermode": smooth_lighting,
+    "dimension": "overworld",
+}
+renders["worlddaysouth"] = {
+    "world": "world",
+    "title": "World Daytime South",
+    "rendermode": smooth_lighting,
+    "dimension": "overworld",
+    "northdirection" : "lower-right",
+}
+renders["worldnight"] = {
+    "world": "world",
+    "title": "World Nighttime",
+    "rendermode": smooth_night,
+    "dimension": "overworld",
+}
+renders["worldnightsouth"] = {
+    "world": "world",
+    "title": "World Nighttime South",
+    "rendermode": smooth_night,
+    "dimension": "overworld",
+    "northdirection" : "lower-right",
+}
+renders["worldcave"] = {
+    "world": "world",
+    "title": "World Caves",
+    "rendermode": cave,
+    "dimension": "overworld",
+}
+
+
+renders["nether"] = {
+    "world": "nether",
+    "title": "Nether",
+    "rendermode": nether_smooth_lighting,
+    "dimension": "nether",
+}
+renders["nethersouth"] = {
+    "world": "nether",
+    "title": "Nether South",
+    "rendermode": nether_smooth_lighting,
+    "dimension": "nether",
+    "northdirection" : "lower-right",
+}
+renders["nethercave"] = {
+    "world": "nether",
+    "title": "Nether Caves",
+    "rendermode": cave,
+    "dimension": "nether",
+}
+
+
+renders["end"] = {
+    "world": "end",
+    "title": "End",
+    "rendermode": smooth_lighting,
+    "dimension": "end",
+}
+renders["endsouth"] = {
+    "world": "end",
+    "title": "End South",
+    "rendermode": smooth_lighting,
+    "dimension": "end",
+    "northdirection" : "lower-right",
+}
+renders["endcave"] = {
+    "world": "end",
+    "title": "End Caves",
+    "rendermode": cave,
+    "dimension": "end",
+}
+
+
+renders["hothday"] = {
+    "world": "hoth",
+    "title": "Hoth Daytime",
+    "rendermode": smooth_lighting,
+    "dimension": "overworld",
+}
+renders["hothdaysouth"] = {
+    "world": "hoth",
+    "title": "Hoth Daytime South",
+    "rendermode": smooth_lighting,
+    "dimension": "overworld",
+    "northdirection" : "lower-right",
+}
+renders["hothnight"] = {
+    "world": "hoth",
+    "title": "Hoth Nighttime",
+    "rendermode": smooth_night,
+    "dimension": "overworld",
+}
+renders["hothnightsouth"] = {
+    "world": "hoth",
+    "title": "hoth Nighttime South",
+    "rendermode": smooth_night,
+    "dimension": "overworld",
+    "northdirection" : "lower-right",
+}
+renders["hothcave"] = {
+    "world": "hoth",
+    "title": "Hoth Caves",
+    "rendermode": cave,
+    "dimension": "overworld",
+}
+
+
+renders["ampday"] = {
+    "world": "amp",
+    "title": "Amplified Daytime",
+    "rendermode": smooth_lighting,
+    "dimension": "overworld",
+}
+renders["ampdaysouth"] = {
+    "world": "amp",
+    "title": "Amplified Daytime South",
+    "rendermode": smooth_lighting,
+    "dimension": "overworld",
+    "northdirection" : "lower-right",
+}
+renders["ampnight"] = {
+    "world": "amp",
+    "title": "Amplified Nighttime",
+    "rendermode": smooth_night,
+    "dimension": "overworld",
+}
+renders["ampnightsouth"] = {
+    "world": "amp",
+    "title": "Amplified Nighttime South",
+    "rendermode": smooth_night,
+    "dimension": "overworld",
+    "northdirection" : "lower-right",
+}
+renders["ampcave"] = {
+    "world": "amp",
+    "title": "Amplified Caves",
+    "rendermode": cave,
+    "dimension": "overworld",
+}
+
+
+renders["oldworldday"] = {
+    "world": "oldworld",
+    "title": "Old World Daytime",
+    "rendermode": smooth_lighting,
+    "dimension": "overworld",
+}
+renders["oldworlddaysouth"] = {
+    "world": "oldworld",
+    "title": "Old World Daytime South",
+    "rendermode": smooth_lighting,
+    "dimension": "overworld",
+    "northdirection" : "lower-right",
+}
+renders["oldworldnight"] = {
+    "world": "oldworld",
+    "title": "Old World Nighttime",
+    "rendermode": smooth_night,
+    "dimension": "overworld",
+}
+renders["oldworldnightsouth"] = {
+    "world": "oldworld",
+    "title": "Old World Nighttime South",
+    "rendermode": smooth_night,
+    "dimension": "overworld",
+    "northdirection" : "lower-right",
+}
+renders["oldworldcave"] = {
+    "world": "oldworld",
+    "title": "Old World Caves",
+    "rendermode": cave,
+    "dimension": "overworld",
+}
+
+
+renders["oldnether"] = {
+    "world": "oldnether",
+    "title": "Old Nether",
+    "rendermode": nether_smooth_lighting,
+    "dimension": "nether",
+}
+renders["oldnethersouth"] = {
+    "world": "oldnether",
+    "title": "Old Nether South",
+    "rendermode": nether_smooth_lighting,
+    "dimension": "nether",
+    "northdirection" : "lower-right",
+}
+renders["oldnethercave"] = {
+    "world": "oldnether",
+    "title": "Old Nether Caves",
+    "rendermode": cave,
+    "dimension": "nether",
+}
+
+
+renders["oldend"] = {
+    "world": "oldend",
+    "title": "Old End",
+    "rendermode": smooth_lighting,
+    "dimension": "end",
+}
+renders["oldendsouth"] = {
+    "world": "oldend",
+    "title": "End South",
+    "rendermode": smooth_lighting,
+    "dimension": "end",
+    "northdirection" : "lower-right",
+}
+renders["oldendcave"] = {
+    "world": "oldend",
+    "title": "End Caves",
+    "rendermode": cave,
+    "dimension": "end",
+}
+
+
+outputdir = "/home/zacharydubois/render/done"
+texturepath = "/home/zacharydubois/render/texturepack.zip"
